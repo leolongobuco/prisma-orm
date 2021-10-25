4d057ceae9227dded96e79d23ce4b78bc5394dfe
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-10-25 17:40:16 -0300

    Store do controller de User

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import teamRoutes from "./routes/teamRoutes";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
+
 app.use(express.json());
 app.use("/teams", teamRoutes);
+app.use("/users", userRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`App running on PORT ${process.env.PORT} 🚀`);
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,1 +1,28 @@
+import { Request, Response } from "express";
+import bcrypt from "bcryptjs";
+
 import prisma from "../config/prismaClient";
+
+class User {
+  async store(request: Request, response: Response) {
+    const { email, password } = request.body;
+
+    try {
+      const passwordHash = await bcrypt.hash(password, 8);
+      const user = await prisma.user.create({
+        data: {
+          email,
+          password: passwordHash,
+        },
+      });
+
+      const { createdAt } = user;
+
+      return response.status(201).json({ email, createdAt });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+}
+
+export default new User();
