16fcb3d48cc8bd9943d412f73ec9fe195b308409
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-10-24 23:09:37 -0300

    CRUD de times finalizado

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
 import prisma from "../config/prismaClient";
 
-// TODO: SHOW UPDATE DELETE
 class TeamController {
   async store(request: Request, response: Response) {
     const { name, state, city, foundation, stadium, colorTeam } = request.body;
@@ -33,11 +32,67 @@ class TeamController {
     }
   }
 
-  // show(request: Request, response: Response) {
+  async show(request: Request, response: Response) {
+    const { teamId } = request.params;
 
-  // update(request: Request, response: Response) {}
+    try {
+      const team = await prisma.teams.findUnique({
+        where: {
+          id: Number(teamId),
+        },
+      });
+      return response.status(200).json(team);
+    } catch (error) {
+      return response.status(404).json({ errors: error.message });
+    }
+  }
 
-  // delete(request: Request, response: Response) {}
+  async update(request: Request, response: Response) {
+    const { teamId } = request.params;
+    const { name, state, city, foundation, stadium, colorTeam } = request.body;
+
+    if (!teamId) {
+      return response.status(404).json({ error: ["Missing Id"] });
+    }
+
+    try {
+      const newDataTeam = await prisma.teams.update({
+        where: {
+          id: Number(teamId),
+        },
+        data: {
+          name,
+          state,
+          city,
+          foundation,
+          stadium,
+          colorTeam,
+        },
+      });
+      return response.status(200).json(newDataTeam);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
+  }
+
+  async delete(request: Request, response: Response) {
+    const { teamId } = request.params;
+
+    if (!teamId) {
+      return response.status(404).json({ error: ["Missing Id"] });
+    }
+
+    try {
+      await prisma.teams.delete({
+        where: {
+          id: Number(teamId),
+        },
+      });
+      return response.status(204).json(null);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
+  }
 }
 
 export default new TeamController();
