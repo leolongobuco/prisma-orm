d95cd557cbe0f31c34d1eba48ae02133013d821c
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-10-25 23:06:11 -0300

    CRUD de Users

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -9,7 +9,7 @@ class User {
 
     try {
       const passwordHash = await bcrypt.hash(password, 8);
-      const user = await prisma.user.create({
+      const user = await prisma.users.create({
         data: {
           email,
           password: passwordHash,
@@ -19,6 +19,67 @@ class User {
       const { createdAt } = user;
 
       return response.status(201).json({ email, createdAt });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  async index(request: Request, response: Response) {
+    try {
+      const users = await prisma.users.findMany();
+
+      return response.status(200).json({ users });
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
+  }
+
+  async show(request: Request, response: Response) {
+    const { userId } = request.params;
+
+    try {
+      const user = await prisma.users.findUnique({
+        where: {
+          id: Number(userId),
+        },
+      });
+      const { email } = user;
+      return response.status(200).json(email);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
+  }
+
+  async update(request: Request, response: Response) {
+    const { userId } = request.params;
+    const { password } = request.body;
+
+    try {
+      const newDataUser = await prisma.users.update({
+        where: {
+          id: Number(userId),
+        },
+        data: {
+          password,
+        },
+      });
+      const { id, email, createdAt, updatedAt } = newDataUser;
+      return response.status(200).json({ id, email, createdAt, updatedAt });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  }
+
+  async delete(request: Request, response: Response) {
+    const { userId } = request.params;
+
+    try {
+      await prisma.users.delete({
+        where: {
+          id: Number(userId),
+        },
+      });
+      return response.status(204).json(null);
     } catch (error) {
       return response.status(400).json({ error: error.message });
     }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,5 +4,9 @@ import UserController from "../controllers/UserController";
 const router = Router();
 
 router.post("/", UserController.store);
+router.get("/", UserController.index);
+router.get("/:userId", UserController.show);
+router.put("/:userId", UserController.update);
+router.delete("/:userId", UserController.delete);
 
 export default router;
