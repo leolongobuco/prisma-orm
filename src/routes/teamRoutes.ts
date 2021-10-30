0d35bc9d5aad4c05ce5d44bcfc57ece4374e5e28
Leonardo Longobuco <longobucoleonardo@gmail.com>
2021-10-29 23:12:12 -0300

    Ajustes de conflito

--- src/routes/teamRoutes.ts
+++ src/routes/teamRoutes.ts
@@ -7,12 +7,7 @@ const router = Router();
 router.post("/", requireAuth, TeamController.store);
 router.get("/", TeamController.index);
 router.get("/:teamId", TeamController.show);
-<<<<<<< HEAD
-router.put("/:teamId", TeamController.update);
-router.delete("/:teamId", TeamController.delete);
-=======
 router.put("/:teamId", requireAuth, TeamController.update);
 router.delete("/:teamId", requireAuth, TeamController.delete);
->>>>>>> develop
 
 export default router;
